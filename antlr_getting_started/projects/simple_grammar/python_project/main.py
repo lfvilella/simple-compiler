fc396f1f4293811ff7c92439316d2df3a473f5dc
Luis Felipe <vilella.luisfelipe@gmail.com>
2021-11-08 19:54:21 -0300

    antlr created MyErrorListener

--- antlr_getting_started/projects/simple_grammar/python_project/main.py
+++ antlr_getting_started/projects/simple_grammar/python_project/main.py
@@ -1,9 +1,17 @@
 import click
 from antlr4 import *
+from antlr4.error.ErrorListener import ErrorListener
 
 from Grammar.SimpleGrammarLexer import SimpleGrammarLexer
 from Grammar.SimpleGrammarParser import SimpleGrammarParser
 from Grammar.SimpleGrammarListener import SimpleGrammarListener
+
+
+class MyErrorListener(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        print(
+            f'Coé, erro na linha {line}:{column}. Simbolo nada ve esse: {offendingSymbol.text}'
+        )
 
 
 class MyGrammarListener(SimpleGrammarListener):
@@ -21,10 +29,12 @@ def main(filepath):
     lexer = SimpleGrammarLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = SimpleGrammarParser(stream)
+    parser.removeErrorListeners()
+    parser.addErrorListener(listener=MyErrorListener())
     tree = parser.prog()
-    printer = MyGrammarListener()
-    walker = ParseTreeWalker()
-    walker.walk(printer, tree)
+    # printer = MyGrammarListener()
+    # walker = ParseTreeWalker()
+    # walker.walk(printer, tree)
 
 
 if __name__ == '__main__':
